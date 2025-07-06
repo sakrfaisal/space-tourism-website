c44caaab0ff5c1d915b8d7ba1981994a31035c67
Sakhr <sakr1772007@gmail.com>
2025-07-06 20:19:17 +0300

    enhance routin and update config for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   return (
-    <>
+    <BrowserRouter basename="space-tourism-website">
       <AppRoutes />
-    </>
+    </BrowserRouter>
   );
 }
 
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/space-tourism-website",
+  base: "/space-tourism-website/",
   plugins: [react(), tailwindcss()],
 });
